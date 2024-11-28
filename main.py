78ef00bbda7f1893df69629b64bc86aee261f70a
hat3ik <rayskiy068@gmail.com>
2024-11-28 20:22:14 +0400

    Second commit

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 class MyClass:
-    """
-    Описание класса MyClass
-    """
     def __init__(self, name):
-        """
-         инициализаия класса
-        """
         self.name = name
 
     def say_hello(self):
-        """
-        Метод для приветствия
-        """
-        print(f"Привет {self.name}")
+        print(f"грывгарывфгра")
